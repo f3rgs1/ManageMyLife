b11a9071fb69fc807b55b219bbee201a48c01830
f3rgs1 <minervax7@protonmail.ch>
2021-01-21 01:27:42 +0000

    Added basics to 'Mange' (I know) my life

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+#sets root window object
+root = Tk()
+#sets title
+root.title("Manage My Life")
+#sets default size of window
+root.geometry("700x350")
+#runs program
+root.mainloop()
