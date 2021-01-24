069d9c54396ee9e5fa5359299889a1900d7682cd
f3rgs1 <minervax7@protonmail.ch>
2021-01-24 00:10:17 +0000

    updated main.py

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from tkinter import *
+import tkinter as tk
 
-#sets root window object
-root = Tk()
-#sets title
-root.title("Manage My Life")
-#sets default size of window
-root.geometry("700x350")
-#runs program
-root.mainloop()
+# sets root window object
+window = tk.Tk()
+# sets title
+window.title("Manage My Life")
+# sets default size of window
+window.geometry("700x350")
+
+# adds a widget to the GUI
+greetingwidget = tk.Label(text="Greetings!")
+greetingwidget.pack()
+# blocks program, any code after here will not run. Consider using update().
+window.mainloop()
